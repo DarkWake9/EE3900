c7fb34cf6ce5ded5a26cd1c19ac0be9ebc0fe4cc
DarkWake9 <vibhavasu2018@gmail.com>
2022-09-25 21:49:23 +0530

    25092022 - 21.49 - Q6,Q7 UPDATED

--- Assignment 1/yndft.py
+++ Assignment 1/yndft.py
@@ -6,7 +6,7 @@ import shlex
 #end if
 
 
-N = 14
+N = 16
 n = np.arange(N)
 fn=(-1/2)**n
 hn1=np.pad(fn, (0,2), 'constant', constant_values=(0))
@@ -14,7 +14,7 @@ hn2=np.pad(fn, (2,0), 'constant', constant_values=(0))
 h = hn1+hn2
 
 xtemp=np.array([1.0,2.0,3.0,4.0,2.0,1.0])
-x=np.pad(xtemp, (0,8), 'constant', constant_values=(0))
+x=np.pad(xtemp, (0,N-6), 'constant', constant_values=(0))
 
 X = np.zeros(N) + 1j*np.zeros(N)
 for k in range(0,N):
@@ -43,24 +43,47 @@ plt.stem(range(0,N),X)
 plt.title('$X(k)$ with DFT')
 plt.xlabel('$n$')
 plt.ylabel('$X(k)$')
-plt.grid()# minor
-plt.savefig("Assignment 1/fig/Xkdft.jpg")
+plt.grid(True, which='both')# minor
+plt.savefig("Assignment 1/filter/figs/Xkdft.jpg")
 plt.show()
 H = np.real(H)
 plt.stem(range(0,N),H)
 plt.title('$H(k)$ with DFT')
 plt.xlabel('$n$')
 plt.ylabel('$H(k)$')
-plt.grid()# minor
-plt.savefig("Assignment 1/fig/Hkdft.jpg")
+plt.grid(True, which='both')# minor
+plt.savefig("Assignment 1/filter/figs/Hkdft.jpg")
+plt.show()
+
+
+
+fig = plt.figure(figsize=(8,6))
+plt.subplot(2,1,1)
+plt.stem(range(0,N),X)
+plt.ylabel('$y(n)$')
+plt.grid()
+plt.title('X(k) using DFT')
+
+plt.subplot(2,1,2)
+plt.stem(range(0,N),H)
+plt.xlabel('$k$')
+plt.ylabel('$y(n)$')
+plt.title('H(k) using DFT')
+plt.grid()
+plt.savefig('Assignment 1/filter/figs/e6.1DFT.jpg',facecolor=fig.get_facecolor(), edgecolor='none')
+plt.show()
+
+
+
 Y = np.real(Y)
 plt.stem(range(0,N),Y)
 plt.title('$Y(k)$ with DFT')
 plt.xlabel('$n$')
 plt.ylabel('$Y(k)$')
-plt.grid()# minor
-plt.savefig("Assignment 1/fig/Ykdft.jpg")
+plt.grid(True, which='both')# minor
+plt.savefig("Assignment 1/filter/figs/Ykdft.jpg")
 plt.show()
+
 y = np.real(y)/N
 print("\nRe{y}\n")
 print(y)
