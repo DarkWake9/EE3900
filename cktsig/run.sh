9757fc942ad84336f41b919016b493fdee59220c
DarkWake9 <vibhavasu2018@gmail.com>
2022-11-06 16:11:34 +0530

    06112022 - 16.11 - BASH SCRIPT UPDATED

--- cktsig/run.sh
+++ cktsig/run.sh
@@ -1,15 +1,29 @@
+echo VIBHAVASU - EP20BTECH11015
+echo THIS IS THE EXECUTION BASH SCRIPT FOR CKTSIG ASSIGNMENT
+
 cd codes
+echo Running python codes in
 pwd
+
 python3 e2.6.py
+echo e2.6.py ran successfully
 
 python3 e3.4.py
+echo e3.4.py ran successfully
 
 python3 e4.3.py
+echo e4.3.py ran successfully
 
 python3 e4.7.py
-pwd
+echo e4.7.py ran successfully
+
+echo All python codes ran successfully
+
 cd ..
 
-pdflatex main.tex
+echo Compiling tex file in
+pwd
 
+pdflatex main.tex
 xdg-open main.pdf
+echo THANK YOU
